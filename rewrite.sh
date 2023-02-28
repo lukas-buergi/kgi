5d4b0c3056ae0a1af504e1845b3e3483f8343328
Lukas Bürgi <buergi.lukas@gmail.com>
2023-02-28 21:33:36 +0100

    probably fixed rewrite script rewriting itself...

--- rewrite.sh
+++ rewrite.sh
@@ -5,11 +5,11 @@ prefix="file://wsl.localhost/Debian/home/t4b/work/gsoa/kgi-website-static/kriegs
 find . -name '*\?ver=*' | sort | sed -E 's/(.*)\?(.*)/cp \1?\2 \1/' > tmp.sh
 bash tmp.sh
 
-abs='-e s|href="https?://(www.)?kriegsgeschaefte.ch/|href="'
+abs='-e s|(href|src)="https?://(www.)?kriegsgeschaefte.ch/|\1="'
 abs+=$prefix
 abs+='|g'
 
-root='-e s|href="/|href="'
+root='-e s|(href|src)="/|\1="'
 root+=$prefix
 root+='|g'
-grep -rl 'href="' . | xargs -L 1 --verbose sed -i $abs $root
+grep -rl 'href="' *.ch | xargs -L 1 --verbose sed -i $abs $root
